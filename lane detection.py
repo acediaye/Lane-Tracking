107e345f742763d5b728a81b00df1df3caddd94a
Andy <ayeh616@gmail.com>
2021-06-17 23:01:26 -0400

    trying to find left and right line

--- lane detection.py
+++ lane detection.py
@@ -20,17 +20,45 @@ whos(image)
 
 myimage = blackwhite(image)
 # print(myimage)
-whos(myimage)
-myimage = sobel(myimage)
-whos(myimage)
-myimage = region_of_interest(myimage)
-whos(myimage)
+# whos(myimage)
+# myimage = sobel(myimage)  # slow
+# whos(myimage)
+# myimage = region_of_interest(myimage)
+# whos(myimage)
 
-# # plt.imsave('pics/mine.jpg', myimage, cmap='gray')
-# # myimage = plt.imread('pics/mine.jpg')
+# # # plt.imsave('pics/mine.jpg', myimage, cmap='gray')
+# # # myimage = plt.imread('pics/mine.jpg')
 
-myimage = hough(myimage)
-whos(myimage)
+# accumulator, thetas, rhos = hough(myimage)  # slow
+# myimage = inverse_hough(myimage, accumulator, thetas, rhos)
+# whos(myimage)
+# find_left_right_line(myimage, accumulator, thetas, rhos)
 
+p1 = [1309, 704]
+p2 = [1176, 422]
+pp1 = [817, 704]
+pp2 = [929, 422]
+plt.plot([1309, 1176], [704, 422], 'ro-')
+plt.plot([817, 929], [704, 422], 'go-')
 plt.imshow(myimage, cmap='gray')
 plt.show()
+
+# m1, b1 = [-2.11442821e+00, -2.06536814e+03]  # 14
+# m2, b2 = [2.51428030e+00, 2.76010084e+03]
+# # x1 = 1000
+# # x2 = 1000
+# # y1 = m1*x1 + b1
+# # y2 = m2*x2 + b2
+# y1 = 704  # 704
+# y2 = int(y1*(3/5))  # 424
+# x1 = int(-(y1 - b1)/m1)
+# x2 = int(-(y2 - b1)/m1)
+# print(x1, y1)
+# print(x2, y2)
+# xx1 = int(-(y1 - b2)/m2)
+# xx2 = int(-(y2 - b2)/m2)
+# print(xx1, y1)
+# print(xx2, y2)
+# plt.plot([x1, x2], [y1, y2], 'ro-')
+# plt.plot([xx1, xx2], [y1, y2], 'go-')
+# plt.show()
--- library.py
+++ library.py
@@ -26,17 +26,17 @@ def sobel(image: np.ndarray):
     height, width = np.shape(image)
     output = np.zeros(shape=(height, width), dtype=np.float32)
 
-    for r in range(1, height-1):
-        for c in range(1, width-1):
-            Sx = np.sum(Gx*image[r-1:r+2, c-1:c+2])
-            Sy = np.sum(Gy*image[r-1:r+2, c-1:c+2])
+    for y in range(1, height-1):
+        for x in range(1, width-1):
+            sum_x = np.sum(Gx*image[y-1:y+2, x-1:x+2])
+            sum_y = np.sum(Gy*image[y-1:y+2, x-1:x+2])
             # print(type(S1), S1)
-            mag = round(np.sqrt(Sx**2 + Sy**2))
-            theta = np.arctan2(Sy, Sx)
+            mag = round(np.sqrt(sum_x**2 + sum_y**2))
+            theta = np.arctan2(sum_y, sum_x)
             if mag >= threshold:
-                output[r, c] = mag
+                output[y, x] = mag
             else:
-                output[r, c] = 0
+                output[y, x] = 0
     # print(np.amax(output), np.amin(output))
     return output
 
@@ -53,14 +53,14 @@ def region_of_interest(image: np.ndarray):
     # y1 = left_slope*x + height
     # y2 = right_slope*x
     output = np.zeros(shape=(height, width), dtype=np.float32)
-    for r in range(height):
-        for c in range(width):
-            y1 = left_slope*c + height
-            y2 = right_slope*c
-            if r > y1 and r > y2:
-                output[r, c] = 1.0 and image[r, c]  # white
+    for y in range(height):
+        for x in range(width):
+            y_left = left_slope*x + height
+            y_right = right_slope*x
+            if y > y_left and y > y_right:
+                output[y, x] = 1.0 and image[y, x]  # white
             else:
-                output[r, c] = 0 and image[r, c]  # black
+                output[y, x] = 0 and image[y, x]  # black
     return output
 
 
@@ -91,13 +91,10 @@ def hough(image: np.ndarray):
     maxdist = round(np.sqrt(height**2 + width**2))  # max distance
     thetas = np.deg2rad(range(-90, 90, 1))  # range of thetas
     rhos = range(-maxdist, maxdist, 1)  # range of rhos
-    # print(maxdist)
     accumulator = np.zeros(shape=(len(rhos), len(thetas)), dtype=np.float32)
-    count = 0
     for y in range(height):
         for x in range(width):
             if image[y, x] > 0:
-                # count += 1
                 for i in range(len(thetas)):  # for each theta
                     rho = (x*np.cos(thetas[i])
                            + y*np.sin(thetas[i]))  # compute rho
@@ -107,7 +104,12 @@ def hough(image: np.ndarray):
     # temp = ceiling(accumulator)
     # plt.imshow(temp, cmap='gray', aspect='auto')
     # plt.show()
+    return accumulator, thetas, rhos
 
+
+def inverse_hough(image: np.ndarray, accumulator: np.ndarray,
+                  thetas: np.ndarray, rhos: np.ndarray):
+    height, width = np.shape(image)
     threshold = 1 * 0.8
     a_height, a_width = np.shape(accumulator)
     output = np.zeros(shape=(height, width), dtype=np.float32)
@@ -121,28 +123,46 @@ def hough(image: np.ndarray):
                       f'\trow: {r}, col: {c}')
 
                 if theta == 0:  # for vertical line
-                    # x = (width)*[rho]  # list of x
                     y = range(0, height)  # list of y
                     for i in range(height):
                         output[y[i], rho] = 1.0
-                        # image[y[i], rho] = 1.0
                 else:  # any other line
                     x = range(0, width)  # list of x
                     for i in range(width):
                         y = round((rho-x[i]*np.cos(theta))
                                   / np.sin(theta))  # calc y
-                        # print(x[i], y)  # +1278, -1535
                         if y >= 0 and y <= height-1:
-                            count += 1
                             output[y, x[i]] = 1.0
-                            # image[y, x[i]] = 1.0
-
-                # x = rho*np.cos(np.deg2rad(theta))
-                # y = rho*np.sin(np.deg2rad(theta))
-                # print(x, y)
-                # count += 1
-                # output[round(y), round(x)] = 1.0
-    print(count)
     # plt.imshow(output, cmap='gray')
     # plt.show()
     return output
+
+
+def find_left_right_line(image: np.ndarray, accumulator: np.ndarray,
+                         thetas: np.ndarray, rhos: np.ndarray):
+    height, width = np.shape(image)
+    a_height, a_width = np.shape(accumulator)
+    output = np.zeros(shape=(height, width), dtype=np.float32)
+    left_fit = []
+    right_fit = []
+    for r in range(a_height):
+        for c in range(a_width):
+            value = accumulator[r, c]  # at each point
+            if value > 0:
+                rho = rhos[r]  # fetch rho
+                theta = thetas[c]  # fetch theta
+                if rho*np.cos(theta) != 0 and np.sin(theta) != 0:  # divide 0
+                    m = round((rho*np.sin(theta)) / (rho*np.cos(theta)), 10)
+                    y_intercept = round(rho / np.sin(theta), 10)
+                    if m < 0:  # left line has neg slope
+                        left_fit.append((m, y_intercept))
+                    elif m > 0:  # right line has pos slope
+                        right_fit.append((m, y_intercept))
+                    else:
+                        pass
+    print(np.shape(left_fit))
+    print(np.shape(right_fit))
+    average_left_fit = np.average(left_fit, axis=0)
+    average_right_fit = np.average(right_fit, axis=0)
+    print(average_left_fit)
+    print(average_right_fit)
